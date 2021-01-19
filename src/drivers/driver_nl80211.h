abc4a6b6ebd93e5763eea4c0ea2bb782a6d93b42
Jiani Liu <jianil@codeaurora.org>
2021-01-19 15:13:14 +0800

    nl80211: Remove QCA vendor specific AKM capability handling
    Since this functionality was not used for anything in practice, it is
    easier to simply remove this functionality completely to avoid potential
    conflicts in using the kernel tree upstream commit ab4dfa20534e
    ("cfg80211: Allow drivers to advertise supported AKM suites").
    
    This is practically reverting the commit 8ec7c99ee4c6 ("nl80211: Fetch
    supported AKM list from the driver").
    
    Signed-off-by: Veerendranath Jakkam <vjakkam@codeaurora.org>
    
    Git-repo: git://w1.fi/srv/git/hostap.git
    Git-commit: 6fffb320fc278bd0af091e3ee53417cdead6ac6a
    CRs-Fixed: 2857673
    Change-Id: I7ca529166b041a2cf9b1cc1c4bf5d6bf509b68c7

--- src/drivers/driver_nl80211.h
+++ src/drivers/driver_nl80211.h
@@ -172,7 +172,6 @@ struct wpa_driver_nl80211_data {
 	unsigned int set_wifi_conf_vendor_cmd_avail:1;
 	unsigned int fetch_bss_trans_status:1;
 	unsigned int roam_vendor_cmd_avail:1;
-	unsigned int get_supported_akm_suites_avail:1;
 
 	u64 vendor_scan_cookie;
 	u64 remain_on_chan_cookie;
