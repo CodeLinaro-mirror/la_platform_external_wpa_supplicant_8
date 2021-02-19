9945b7695f4c28e7fe9a6297d6acdd93a7831a52
Chenming Huang <chenhuan@codeaurora.org>
2021-02-19 14:03:34 +0800

    wifi: set he_oper_chwidth=1
    CRs-Fixed: 2846188
    Change-Id: I3ad4333425bbd92b5d2d2ca7d898199b9c42b6df

--- hostapd/hidl/1.2/hostapd.cpp
+++ hostapd/hidl/1.2/hostapd.cpp
@@ -489,6 +489,7 @@ std::string CreateHostapdConfig(
 	if (iface_params.hwModeParams.enable80211AX) {
 		he_params_as_string = StringPrintf(
 		    "ieee80211ax=1\n"
+		    "he_oper_chwidth=1\n"
 		    "he_su_beamformer=%d\n"
 		    "he_su_beamformee=%d\n"
 		    "he_mu_beamformer=%d\n"
