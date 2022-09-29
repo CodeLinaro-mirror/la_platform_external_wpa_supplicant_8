175d504cc4be9191743c972b8b5db0b42cc2584d
Shivani Baranwal <quic_shivbara@quicinc.com>
2022-09-30 00:11:42 +0530

    hostapd:Merge MLO SAP single link support for hostapd
    00d14f820 AIDL: Add support to enable 11BE single link MLD SAP from UI
    8c52848e3 EHT: External SAE authentication support for single link MLD AP
    481df2934 EHT: OWE support for single link MLD AP when SME offload to driver
    1317a37a6 EHT: Support WPA2 PSK mode for single link MLD AP when SME offload to driver
    709801168 EHT: Open mode support for single link AP MLD when SME offload to driver
    d9abfc01e EHT: Add support for single link MLD AP start and stop
    
    Change-Id: I0a38666907100f78fde81a5bd4ff95179f2881f8

--- src/ap/drv_callbacks.c
+++ src/ap/drv_callbacks.c
@@ -132,7 +132,9 @@ static bool check_sa_query_need(struct hostapd_data *hapd, struct sta_info *sta)
 
 
 int hostapd_notif_assoc(struct hostapd_data *hapd, const u8 *addr,
-			const u8 *req_ies, size_t req_ies_len, int reassoc)
+			const u8 *req_ies, size_t req_ies_len,
+			const u8 *link_addr, const u8 *resp_ies,
+			size_t resp_ies_len, int reassoc)
 {
 	struct sta_info *sta;
 	int new_assoc;
@@ -213,7 +215,7 @@ int hostapd_notif_assoc(struct hostapd_data *hapd, const u8 *addr,
 		 */
 		sta->timeout_next = STA_NULLFUNC;
 	} else {
-		sta = ap_sta_add(hapd, addr);
+		sta = ap_sta_add(hapd, addr, link_addr);
 		if (sta == NULL) {
 			hostapd_drv_sta_disassoc(hapd, addr,
 						 WLAN_REASON_DISASSOC_AP_BUSY);
@@ -343,12 +345,15 @@ int hostapd_notif_assoc(struct hostapd_data *hapd, const u8 *addr,
 		if (sta->wpa_sm == NULL)
 			sta->wpa_sm = wpa_auth_sta_init(hapd->wpa_auth,
 							sta->addr,
+							sta->is_mld ?
+							sta->link_addr : NULL,
 							p2p_dev_addr);
 		if (sta->wpa_sm == NULL) {
 			wpa_printf(MSG_ERROR,
 				   "Failed to initialize WPA state machine");
 			return -1;
 		}
+
 		res = wpa_validate_wpa_ie(hapd->wpa_auth, sta->wpa_sm,
 					  hapd->iface->freq,
 					  ie, ielen,
@@ -505,7 +510,7 @@ int hostapd_notif_assoc(struct hostapd_data *hapd, const u8 *addr,
 		wpa_printf(MSG_DEBUG, "HS 2.0: OSEN association");
 		if (sta->wpa_sm == NULL)
 			sta->wpa_sm = wpa_auth_sta_init(hapd->wpa_auth,
-							sta->addr, NULL);
+							sta->addr, NULL, NULL);
 		if (sta->wpa_sm == NULL) {
 			wpa_printf(MSG_WARNING,
 				   "Failed to initialize WPA state machine");
@@ -1282,7 +1287,7 @@ static void hostapd_notif_auth(struct hostapd_data *hapd,
 
 	sta = ap_get_sta(hapd, rx_auth->peer);
 	if (!sta) {
-		sta = ap_sta_add(hapd, rx_auth->peer);
+		sta = ap_sta_add(hapd, rx_auth->peer, NULL);
 		if (sta == NULL) {
 			status = WLAN_STATUS_AP_UNABLE_TO_HANDLE_NEW_STA;
 			goto fail;
@@ -1295,7 +1300,7 @@ static void hostapd_notif_auth(struct hostapd_data *hapd,
 		sta->auth_alg = WLAN_AUTH_FT;
 		if (sta->wpa_sm == NULL)
 			sta->wpa_sm = wpa_auth_sta_init(hapd->wpa_auth,
-							sta->addr, NULL);
+							sta->addr, NULL, NULL);
 		if (sta->wpa_sm == NULL) {
 			wpa_printf(MSG_DEBUG,
 				   "FT: Failed to initialize WPA state machine");
@@ -1547,7 +1552,7 @@ static int hostapd_event_new_sta(struct hostapd_data *hapd, const u8 *addr)
 
 	wpa_printf(MSG_DEBUG, "Data frame from unknown STA " MACSTR
 		   " - adding a new STA", MAC2STR(addr));
-	sta = ap_sta_add(hapd, addr);
+	sta = ap_sta_add(hapd, addr, NULL);
 	if (sta) {
 		hostapd_new_assoc_sta(hapd, sta, 0);
 	} else {
@@ -1811,7 +1816,7 @@ static void hostapd_event_wds_sta_interface_status(struct hostapd_data *hapd,
 #ifdef CONFIG_OWE
 static int hostapd_notif_update_dh_ie(struct hostapd_data *hapd,
 				      const u8 *peer, const u8 *ie,
-				      size_t ie_len)
+				      size_t ie_len, const u8 *link_addr)
 {
 	u16 status;
 	struct sta_info *sta;
@@ -1853,7 +1858,7 @@ static int hostapd_notif_update_dh_ie(struct hostapd_data *hapd,
 		 */
 		sta->timeout_next = STA_NULLFUNC;
 	} else {
-		sta = ap_sta_add(hapd, peer);
+		sta = ap_sta_add(hapd, peer, link_addr);
 		if (!sta) {
 			status = WLAN_STATUS_UNSPECIFIED_FAILURE;
 			goto err;
@@ -1978,6 +1983,9 @@ void wpa_supplicant_event(void *ctx, enum wpa_event_type event,
 		hostapd_notif_assoc(hapd, data->assoc_info.addr,
 				    data->assoc_info.req_ies,
 				    data->assoc_info.req_ies_len,
+				    data->assoc_info.link_addr,
+				    data->assoc_info.resp_ies,
+				    data->assoc_info.resp_ies_len,
 				    data->assoc_info.reassoc);
 		break;
 #ifdef CONFIG_OWE
@@ -1986,7 +1994,8 @@ void wpa_supplicant_event(void *ctx, enum wpa_event_type event,
 			return;
 		hostapd_notif_update_dh_ie(hapd, data->update_dh.peer,
 					   data->update_dh.ie,
-					   data->update_dh.ie_len);
+					   data->update_dh.ie_len,
+					   data->update_dh.link_addr);
 		break;
 #endif /* CONFIG_OWE */
 	case EVENT_DISASSOC:
--- src/drivers/driver_nl80211.h
+++ src/drivers/driver_nl80211.h
@@ -99,6 +99,7 @@ struct i802_bss {
 	struct nl80211_wiphy_data *wiphy_data;
 	struct dl_list wiphy_list;
 	u8 rand_addr[ETH_ALEN];
+	int link_id;
 };
 
 struct drv_nl80211_if_info {
