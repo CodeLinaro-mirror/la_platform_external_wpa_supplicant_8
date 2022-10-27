46b553edd10c73f94d620f1a6550633dc29f0fad
Veerendranath Jakkam <quic_vjakkam@quicinc.com>
2022-10-27 12:30:17 +0530

    EHT: Support WPA2 PSK mode for single link MLD AP
    Add support for WPA2 PSK security mode for single link MLD AP when
    driver's SME in use. Add below changes to support the same:
    - Add link ID to key configuration functions.
    - Add link ID to get sequence number functions.
    - Add MLO MAC KDE with AP MLD address in EAPOL 1/4 and 3/4 frames for
      MLD STA.
    - Add MLO Link and GTK/IGTK/BIGTKs KDEs in EAPOL 3/4 frame for MLD STA.
    
    CRs-Fixed: 3359006
    Change-Id: Ia09ac367891927dea74b7a43ee039293daaba7f6

--- src/ap/ieee802_11.c
+++ src/ap/ieee802_11.c
@@ -1791,7 +1791,7 @@ void handle_auth_fils(struct hostapd_data *hapd, struct sta_info *sta,
 
 	if (!sta->wpa_sm)
 		sta->wpa_sm = wpa_auth_sta_init(hapd->wpa_auth, sta->addr,
-						NULL);
+						NULL, NULL);
 	if (!sta->wpa_sm) {
 		wpa_printf(MSG_DEBUG,
 			   "FILS: Failed to initialize RSN state machine");
@@ -3052,7 +3052,7 @@ static void handle_auth(struct hostapd_data *hapd,
 		sta->auth_alg = WLAN_AUTH_FT;
 		if (sta->wpa_sm == NULL)
 			sta->wpa_sm = wpa_auth_sta_init(hapd->wpa_auth,
-							sta->addr, NULL);
+							sta->addr, NULL, NULL);
 		if (sta->wpa_sm == NULL) {
 			wpa_printf(MSG_DEBUG, "FT: Failed to initialize WPA "
 				   "state machine");
@@ -3075,7 +3075,8 @@ static void handle_auth(struct hostapd_data *hapd,
 			if (sta->wpa_sm == NULL)
 				sta->wpa_sm =
 					wpa_auth_sta_init(hapd->wpa_auth,
-							  sta->addr, NULL);
+							  sta->addr, NULL,
+							  NULL);
 			if (sta->wpa_sm == NULL) {
 				wpa_printf(MSG_DEBUG,
 					   "SAE: Failed to initialize WPA state machine");
@@ -3539,7 +3540,7 @@ u16 owe_process_rsn_ie(struct hostapd_data *hapd,
 
 	if (!sta->wpa_sm)
 		sta->wpa_sm = wpa_auth_sta_init(hapd->wpa_auth,	sta->addr,
-						NULL);
+						NULL, NULL);
 	if (!sta->wpa_sm) {
 		wpa_printf(MSG_WARNING,
 			   "OWE: Failed to initialize WPA state machine");
@@ -3800,7 +3801,7 @@ static int check_assoc_ies(struct hostapd_data *hapd, struct sta_info *sta,
 		wpa_ie_len += 2;
 		if (sta->wpa_sm == NULL)
 			sta->wpa_sm = wpa_auth_sta_init(hapd->wpa_auth,
-							sta->addr,
+							sta->addr, NULL,
 							p2p_dev_addr);
 		if (sta->wpa_sm == NULL) {
 			wpa_printf(MSG_WARNING, "Failed to initialize WPA "
@@ -3948,7 +3949,7 @@ static int check_assoc_ies(struct hostapd_data *hapd, struct sta_info *sta,
 		wpa_printf(MSG_DEBUG, "HS 2.0: OSEN association");
 		if (sta->wpa_sm == NULL)
 			sta->wpa_sm = wpa_auth_sta_init(hapd->wpa_auth,
-							sta->addr, NULL);
+							sta->addr, NULL, NULL);
 		if (sta->wpa_sm == NULL) {
 			wpa_printf(MSG_WARNING, "Failed to initialize WPA "
 				   "state machine");
