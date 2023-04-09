7fae03b616731cca7cfdc2b431baa4cab9997c68
Sai Pratyusha Magam <smagam@qti.qualcomm.com>
2023-04-09 15:58:16 +0530

    qca-hostapd: Add upstream changes for hostad cli termination
    Add upstream changes for hostad cli termination
    
    upstream hostapd commit:
    a4af7962482eedbb674ad3ceb0c7caba0a7ee39f
    Handle signal termination in hostapd_cli for all cases
    
    Change-Id: I510a8eaf52da7c4b5cfbc9f8f225bb1bf9d36280
    (cherry picked from commit 917255bdc2b15b5b3d175655839a97b65b5342c6)
    CRs-Fixed: 4415426

--- hostapd/hostapd_cli.c
+++ hostapd/hostapd_cli.c
@@ -1,6 +1,9 @@
 /*
  * hostapd - command line interface for hostapd daemon
  * Copyright (c) 2004-2022, Jouni Malinen <j@w1.fi>
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  *
  * This software may be distributed under the terms of the BSD license.
  * See README for more details.
@@ -304,6 +307,12 @@ static void hostapd_cli_action_process(char *msg, size_t len)
 	}
 
 	hostapd_cli_exec(action_file, ctrl_ifname, pos);
+}
+
+
+static void hostapd_cli_action_cb(char *msg, size_t len)
+{
+	hostapd_cli_action_process(msg, len);
 }
 
 
@@ -2002,7 +2011,6 @@ static void hostapd_cli_interactive(void)
 			os_snprintf(hfile, hfile_len, "%s/%s", home, fname);
 	}
 
-	eloop_register_signal_terminate(hostapd_cli_eloop_terminate, NULL);
 	edit_init(hostapd_cli_edit_cmd_cb, hostapd_cli_edit_eof_cb,
 		  hostapd_cli_edit_completion_cb, NULL, hfile, NULL);
 	eloop_register_timeout(ping_interval, 0, hostapd_cli_ping, NULL, NULL);
@@ -2026,40 +2034,46 @@ static void hostapd_cli_cleanup(void)
 }
 
 
-static void hostapd_cli_action(struct wpa_ctrl *ctrl)
+static void hostapd_cli_action_ping(void *eloop_ctx, void *timeout_ctx)
 {
-	fd_set rfds;
-	int fd, res;
-	struct timeval tv;
+	struct wpa_ctrl *ctrl = eloop_ctx;
 	char buf[256];
 	size_t len;
 
-	fd = wpa_ctrl_get_fd(ctrl);
-
-	while (!hostapd_cli_quit) {
-		FD_ZERO(&rfds);
-		FD_SET(fd, &rfds);
-		tv.tv_sec = ping_interval;
-		tv.tv_usec = 0;
-		res = select(fd + 1, &rfds, NULL, NULL, &tv);
-		if (res < 0 && errno != EINTR) {
-			perror("select");
-			break;
-		}
-
-		if (FD_ISSET(fd, &rfds))
-			hostapd_cli_recv_pending(ctrl, 0, 1);
-		else {
-			len = sizeof(buf) - 1;
-			if (wpa_ctrl_request(ctrl, "PING", 4, buf, &len,
-					     hostapd_cli_action_process) < 0 ||
-			    len < 4 || os_memcmp(buf, "PONG", 4) != 0) {
-				printf("hostapd did not reply to PING "
-				       "command - exiting\n");
-				break;
-			}
-		}
+	/* verify that connection is still working */
+	len = sizeof(buf) - 1;
+	if (wpa_ctrl_request(ctrl, "PING", 4, buf, &len,
+			     hostapd_cli_action_cb) < 0 ||
+	    len < 4 || os_memcmp(buf, "PONG", 4) != 0) {
+		printf("hostapd did not reply to PING command - exiting\n");
+		eloop_terminate();
+		return;
 	}
+	eloop_register_timeout(ping_interval, 0, hostapd_cli_action_ping,
+			       ctrl, NULL);
+}
+
+
+static void hostapd_cli_action_receive(int sock, void *eloop_ctx,
+				       void *sock_ctx)
+{
+	struct wpa_ctrl *ctrl = eloop_ctx;
+
+	hostapd_cli_recv_pending(ctrl, 0, 1);
+}
+
+
+static void hostapd_cli_action(struct wpa_ctrl *ctrl)
+{
+	int fd;
+
+	fd = wpa_ctrl_get_fd(ctrl);
+	eloop_register_timeout(ping_interval, 0, hostapd_cli_action_ping,
+			       ctrl, NULL);
+	eloop_register_read_sock(fd, hostapd_cli_action_receive, ctrl, NULL);
+	eloop_run();
+	eloop_cancel_timeout(hostapd_cli_action_ping, ctrl, NULL);
+	eloop_unregister_read_sock(fd);
 }
 
 
@@ -2161,6 +2175,8 @@ int main(int argc, char *argv[])
 		os_sleep(1, 0);
 		continue;
 	}
+
+	eloop_register_signal_terminate(hostapd_cli_eloop_terminate, NULL);
 
 	if (action_file && !hostapd_cli_attached)
 		return -1;
