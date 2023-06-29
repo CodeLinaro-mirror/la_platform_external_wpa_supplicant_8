96ca64046cf72a8f35002467c282e97f7fd3c8a2
stonez <quic_stonez@quicinc.com>
2023-06-29 16:47:37 +0800

    wpa_supplicant: propagate from 6e1e115a9 t-keystone-qcom-release
    6e1e115a9 SAE: Add an enum for defining sae_pwe parameter values
    
    Change-Id: Ie53fa66b4a55a7d54f1b09554dcad20510968169
    Git-repo: git://w1.fi/hostap.git
    Git-commit: bbd5a4689b52ea90a803e4aa5e5357938c3d76c9
    Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>
    Signed-off-by: Stone Zhang <quic_stonez@quicinc.com>
    CRs-Fixed: 3503945

--- src/common/defs.h
+++ src/common/defs.h
@@ -475,4 +475,12 @@ enum ptk0_rekey_handling {
 	PTK0_REKEY_ALLOW_NEVER
 };
 
+enum sae_pwe {
+	SAE_PWE_HUNT_AND_PECK = 0,
+	SAE_PWE_HASH_TO_ELEMENT = 1,
+	SAE_PWE_BOTH = 2,
+	SAE_PWE_FORCE_HUNT_AND_PECK = 3,
+	SAE_PWE_NOT_SET = 4,
+};
+
 #endif /* DEFS_H */
