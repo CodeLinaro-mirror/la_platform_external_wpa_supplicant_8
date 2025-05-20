32cdaa14a24ec0e6aca75ff389dc64049e2e1a48
Yifei Zhao <quic_yifezhao@quicinc.com>
2025-05-20 15:43:47 +0800

    hostapd:optimize the SSR logic when CCU hostapd crash
    Change-Id: Iffd483e1437afba80528fff0af6c05f3f686d35c

--- hostapd/aidl/hostapd.cpp
+++ hostapd/aidl/hostapd.cpp
@@ -80,7 +80,6 @@ int band60Ghz = (int)BandMask::BAND_60_GHZ;
 
 int ctrlInterfaceCount = 0;
 bool ap_isexist = false;
-bool is_br_ap = false;
 
 #define MAX_PORTS 1024
 bool GetInterfacesInBridge(std::string br_name,
@@ -1081,6 +1080,11 @@ Hostapd::Hostapd(struct hapd_interfaces* interfaces)
 	const IfaceParams& iface_params,
 	const NetworkParams& nw_params)
 {
+	if(ap_isexist) {
+		wpa_printf(MSG_INFO, "AP already exited, need to be removed and recreated!");
+		removeAccessPointInternal(iface_params.name);
+		terminate();
+	}
 	int channelParamsSize = iface_params.channelParams.size();
 	if (channelParamsSize == 1) {
 		// Single Bridge AP interface(ap_br_wlan2 = vlan + wlan2)
@@ -1165,10 +1169,6 @@ std::vector<uint8_t>  generateRandomOweSsid()
 ::ndk::ScopedAStatus Hostapd::addMultiLinkAccessPoints(
 	const IfaceParams& iface_params, const NetworkParams& nw_params)
 {
-	if(ap_isexist){
-		removeAccessPointInternal(iface_params.name);
-		terminate();
-	}
 	// Get available interfaces in bridge
 	std::vector<std::string> managed_interfaces;
 	std::string br_name = StringPrintf(
@@ -1308,11 +1308,6 @@ std::vector<uint8_t>  generateRandomOweSsid()
 ::ndk::ScopedAStatus Hostapd::addConcurrentAccessPoints(
 	const IfaceParams& iface_params, const NetworkParams& nw_params)
 {
-	if(ap_isexist) {
-		removeAccessPointInternal(iface_params.name);
-		terminate();
-	}
-	is_br_ap = true;
 	int channelParamsListSize = iface_params.channelParams.size();
 	// Get available interfaces in bridge
 	std::vector<std::string> managed_interfaces;
@@ -1356,7 +1351,6 @@ std::vector<uint8_t>  generateRandomOweSsid()
 	// Save bridge interface info
 	br_interfaces_[br_name] = managed_interfaces;
 	ap_isexist = true;
-	is_br_ap = false;
 	return ndk::ScopedAStatus::ok();
 }
 
@@ -1368,10 +1362,6 @@ std::vector<uint8_t>  generateRandomOweSsid()
 	const std::string owe_transition_ifname,
 	bool enable_11be)
 {
-	if(ap_isexist && !is_br_ap) {
-		removeAccessPointInternal(iface_params.name);
-		terminate();
-	}
 	if (hostapd_get_iface(interfaces_, iface_params.name.c_str())) {
 		wpa_printf(
 			MSG_ERROR, "Interface %s already present",
@@ -1486,7 +1476,6 @@ std::vector<uint8_t>  generateRandomOweSsid()
 			iface_params.name.c_str());
 		return createStatus(HostapdStatusCode::FAILURE_UNKNOWN);
 	}
-	ap_isexist = is_br_ap?false:true;
 	return configApInterfaceIP();
 }
 
