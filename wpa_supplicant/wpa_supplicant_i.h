b793899d99a47d00ae60aabb090dc7a70dd95d0e
Jouni Malinen <jouni.malinen@oss.qualcomm.com>
2025-10-09 13:03:39 +0300

    [autocherry] Reduce debug logging of configuration file details in wpa_supplicant am: 42912178b2 am: 42912178b2 am: b5008ab118
    Change-Id: I2732b85e26a4979861f09c34c12694604fe4188f

--- wpa_supplicant/wpa_supplicant_i.h
+++ wpa_supplicant/wpa_supplicant_i.h
@@ -276,6 +276,11 @@ struct wpa_params {
 	 */
 	char *aidl_service_name;
 #endif /* CONFIG_AIDL */
+
+	/**
+	 * show_details - Whether to show config parsing details in debug log
+	 */
+	bool show_details;
 };
 
 struct p2p_srv_bonjour {
@@ -1773,6 +1778,7 @@ struct wpa_supplicant * wpa_supplicant_get_iface(struct wpa_global *global,
 struct wpa_global * wpa_supplicant_init(struct wpa_params *params);
 int wpa_supplicant_run(struct wpa_global *global);
 void wpa_supplicant_deinit(struct wpa_global *global);
+int wpa_supplicant_parse_config(const char *fname);
 
 int wpa_supplicant_scard_init(struct wpa_supplicant *wpa_s,
 			      struct wpa_ssid *ssid);
