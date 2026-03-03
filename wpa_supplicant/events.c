e1dbf33f2b6d390e34585fde718e84cf5bfb2f97
Huang Chenming <chenming.huang@oss.qualcomm.com>
2026-03-03 07:34:56 +0530

    Fix maximum supported link speed calculation
    An issue is found when connecting to an AP with 80 MHz bandwidth
    supporting 4 NSS and 160 MHz bandwidth supporting 2 NSS.
    get_max_nss_capability() simply returns the basic MCS set for HE,
    leading to wrong maximum supported link speed calculation.
    
    Match NSS with bandwidth to calculate the correct maximum supported link
    speed instead.
    
    Signed-off-by: Huang Chenming <chenming.huang@oss.qualcomm.com>
    Git-repo: git://w1.fi/hostap.git
    Git-commit: f5216d806a00ea1c414ed10c99004897bfca0057
    CRs-Fixed: 4411319
    Change-Id: Idd4708fc69ad20b4d1a0dde61161ca69ef42f92a

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -3492,14 +3492,6 @@ static void wpas_parse_connection_info_link(struct wpa_supplicant *wpa_s,
 		return;
 	}
 
-	wpa_s->links[i].max_nss_rx =
-		MIN(get_max_nss_capability(&req_persta_elems, true),
-		    get_max_nss_capability(&resp_persta_elems, false));
-
-	wpa_s->links[i].max_nss_tx =
-		MIN(get_max_nss_capability(&req_persta_elems, false),
-		    get_max_nss_capability(&resp_persta_elems, true));
-
 	sta_cw = get_supported_channel_width(&req_persta_elems);
 	ap_cw = get_operation_channel_width(&resp_persta_elems);
 
@@ -3513,6 +3505,19 @@ static void wpas_parse_connection_info_link(struct wpa_supplicant *wpa_s,
 	} else {
 		wpa_s->links[i].channel_bandwidth = CHAN_WIDTH_20;
 	}
+
+	wpa_s->links[i].max_nss_rx =
+		MIN(get_max_nss_capability(&req_persta_elems, true,
+					   wpa_s->links[i].channel_bandwidth),
+		    get_max_nss_capability(&resp_persta_elems, false,
+					   wpa_s->links[i].channel_bandwidth));
+
+	wpa_s->links[i].max_nss_tx =
+		MIN(get_max_nss_capability(&req_persta_elems, false,
+					   wpa_s->links[i].channel_bandwidth),
+		    get_max_nss_capability(&resp_persta_elems, true,
+					   wpa_s->links[i].channel_bandwidth));
+
 }
 
 
@@ -3551,14 +3556,6 @@ static void wpas_parse_connection_info(struct wpa_supplicant *wpa_s,
 		resp_elems.he_capabilities;
 	wpa_s->connection_eht = req_elems.eht_capabilities &&
 		resp_elems.eht_capabilities;
-	max_nss_rx_req = get_max_nss_capability(&req_elems, 1);
-	max_nss_rx_resp = get_max_nss_capability(&resp_elems, 1);
-	max_nss_tx_req = get_max_nss_capability(&req_elems, 0);
-	max_nss_tx_resp = get_max_nss_capability(&resp_elems, 0);
-	wpa_s->connection_max_nss_rx = (max_nss_tx_resp > max_nss_rx_req) ?
-		max_nss_rx_req : max_nss_tx_resp;
-	wpa_s->connection_max_nss_tx = (max_nss_tx_req > max_nss_rx_resp) ?
-		max_nss_rx_resp : max_nss_tx_req;
 
 	sta_supported_chan_width = get_supported_channel_width(&req_elems);
 	ap_operation_chan_width = get_operation_channel_width(&resp_elems);
@@ -3578,6 +3575,17 @@ static void wpas_parse_connection_info(struct wpa_supplicant *wpa_s,
 	wpa_s->ap_t2lm_negotiation_support =
 				is_ap_t2lm_negotiation_supported(resp_elems.basic_mle,
 				resp_elems.basic_mle_len);
+	max_nss_rx_req = get_max_nss_capability(
+		&req_elems, 1, wpa_s->connection_channel_bandwidth);
+	max_nss_rx_resp = get_max_nss_capability(
+		&resp_elems, 1, wpa_s->connection_channel_bandwidth);
+	max_nss_tx_req = get_max_nss_capability(
+		&req_elems, 0, wpa_s->connection_channel_bandwidth);
+	max_nss_tx_resp = get_max_nss_capability(
+		&resp_elems, 0, wpa_s->connection_channel_bandwidth);
+
+	wpa_s->connection_max_nss_rx = MIN(max_nss_tx_resp, max_nss_rx_req);
+	wpa_s->connection_max_nss_tx = MIN(max_nss_rx_resp, max_nss_tx_req);
 
 	req_mlbuf = ieee802_11_defrag(req_elems.basic_mle,
 				      req_elems.basic_mle_len, true);
