d2ec673ed98406c7972f4439215aa1c94200b698
Meng Yuan <menyua@qti.qualcomm.com>
2026-06-02 17:10:06 +0800

    hostapd: drop STA-initiated GTK rekey requests for WFA WPA3
    Modify the logic in wpa_receive() to silently drop the EAPOL-Key frame
    when it is identified as a GTK rekey request from station to prevent
    transmitting G1 frames.
    
    Change-Id: I9ac08e71b3fb90fc0eeb145e5a8a64578b38e4e4
    (cherry picked from commit 7a443f972730511bc67961bb1b130422169d0a99)

--- src/ap/wpa_auth.h
+++ src/ap/wpa_auth.h
@@ -182,6 +182,7 @@ struct wpa_auth_config {
 	int wpa_gmk_rekey;
 	int wpa_ptk_rekey;
 	int wpa_deny_ptk0_rekey;
+	int ignore_sta_gtk_rekey;
 	u32 wpa_group_update_count;
 	u32 wpa_pairwise_update_count;
 	int wpa_disable_eapol_key_retries;
